1424bd3a6bd2abe9ce5d69578a264f3088faeaf0
Andreas.Abdi <andreasabdi@yahoo.ca>
2017-05-10 11:22:10 -0700

    Finished writing script for downloading file

--- download_image.py
+++ download_image.py
@@ -14,7 +14,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='Download some files from caom2repo')
     parser.add_argument('--period', action='store', dest='period', type=int, default=5,
                         help='minimum period in seconds between request events')
-    parser.add_argument('--url', action='store', dest='url', default='localhost:8080',
+    parser.add_argument('--url', action='store', dest='url', default='http://localhost:8080',
                         help='minimum period in seconds between request events')
     return parser.parse_args()
 
@@ -24,9 +24,9 @@ def repeatedly_download(url, period):
         loop_beginning_time = datetime.now()
         response = requests.get(url)
         print response
-        target_loop_continue_time = loop_beginning_time + timedelta(seconds=period)
-        if target_loop_continue_time > 0:
-            time.sleep(target_loop_continue_time)
+        remaining_time = datetime.now() - loop_beginning_time
+        if remaining_time <= timedelta(seconds=period):
+            time.sleep((timedelta(seconds=period) - remaining_time).total_seconds())
 
 def main():
     "Main function for downloading images from caom2repo"
